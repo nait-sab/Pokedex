2a3ad23c17c0a23c003beb06807733530fdc4aca
decouttere <alexdecouttere3@gmail.com>
2023-01-12 14:39:35 +0100

    notif onclick item

--- app/src/main/java/fr/naitflo/pokedex/pokemon/view/activity/PokemonInfoActivity.kt
+++ app/src/main/java/fr/naitflo/pokedex/pokemon/view/activity/PokemonInfoActivity.kt
@@ -1,18 +1,10 @@
 package fr.naitflo.pokedex.pokemon.view.activity
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.bumptech.glide.Glide
 import fr.naitflo.pokedex.databinding.ActivityPokemonInfoBinding
-import fr.naitflo.pokedex.databinding.ActivityPokemonListeBinding
 import fr.naitflo.pokedex.pokemon.view.model.PokemonPojoUI
 
 
@@ -66,49 +58,9 @@ class PokemonInfoActivity : AppCompatActivity() {
         binding.pokemonInfoFermer.setOnClickListener {
             fermer()
         }
-        binding.pokemonInfoImg.setOnClickListener{
-            sendnotif()
-        }
     }
 
     private fun fermer() {
         startActivity(Intent(this, PokemonListeActivity::class.java))
-    }
-
-    companion object {
-        const val TAG = "CustomNotificationManager"
-        private const val CHANNEL_ID = "demo_purpose"
-        private const val CHANNEL_NAME = "demo purpose channel id"
-        private const val CHANNEL_DESCRIPTION = "This channel will received only demo notification"
-        private const val REQUEST_CODE = 94043
-        private const val NOTIFICATION_ID = 42
-    }
-    private fun sendnotif() {
-        val mNotificationManager: NotificationManager =
-            this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
-        val notificationChannel = NotificationChannel(
-            CHANNEL_ID,
-            CHANNEL_NAME,
-            NotificationManager.IMPORTANCE_DEFAULT
-        ).apply {
-            description = CHANNEL_DESCRIPTION
-        }
-        mNotificationManager.createNotificationChannel(notificationChannel)
-        createNotificationCompatBuilder(binding)
-    }
-    private fun createNotificationCompatBuilder(binding: ActivityPokemonInfoBinding) {
-        val textView: TextView =  findViewById(fr.naitflo.pokedex.R.id.pokemon_info_nom)
-
-
-        val notificationCompat = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setAutoCancel(true)
-            .setContentTitle("#"+binding.pokemonInfoNumero.text+" "+textView.text.toString())
-            .setAutoCancel(true)
-            .setSmallIcon(fr.naitflo.pokedex.R.drawable.ic_pokeball)
-
-        with(NotificationManagerCompat.from(this)) {
-            // notificationId is a unique int for each notification that you must define
-            notify(NOTIFICATION_ID, notificationCompat.build())
-        }
     }
 }
--- app/src/main/java/fr/naitflo/pokedex/pokemon/view/activity/PokemonListeActivity.kt
+++ app/src/main/java/fr/naitflo/pokedex/pokemon/view/activity/PokemonListeActivity.kt
@@ -1,15 +1,22 @@
 package fr.naitflo.pokedex.pokemon.view.activity
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.HapticFeedbackConstants
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import fr.naitflo.pokedex.databinding.ActivityPokemonInfoBinding
 import fr.naitflo.pokedex.databinding.ActivityPokemonListeBinding
 import fr.naitflo.pokedex.firebase.LoginActivity
 import fr.naitflo.pokedex.pokemon.view.adapter.PokemonAdapter
@@ -90,6 +97,43 @@ class PokemonListeActivity : AppCompatActivity() {
         val i = Intent(this, PokemonInfoActivity::class.java)
         val pokemonPojoUI: PokemonPojoUI = pokemonPojoUI
         i.putExtra("donneesPokemon", pokemonPojoUI)
+        sendnotif(pokemonPojoUI)
         startActivity(i)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "item_notif_id"
+        private const val CHANNEL_NAME = "item_notif_name"
+        private const val CHANNEL_DESCRIPTION = "Channel for pokemon notif"
+        private const val NOTIFICATION_ID = 42
+    }
+
+    private fun sendnotif(pokemonPojoUI: PokemonPojoUI) {
+        val mNotificationManager: NotificationManager =
+            this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
+        val notificationChannel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = CHANNEL_DESCRIPTION
+        }
+        mNotificationManager.createNotificationChannel(notificationChannel)
+        createNotificationCompatBuilder(pokemonPojoUI)
+    }
+
+    private fun createNotificationCompatBuilder(pokemonPojoUI: PokemonPojoUI) {
+        val notificationCompat = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setAutoCancel(true)
+            .setContentTitle("#" + pokemonPojoUI.pokedexId.toString() + " " + pokemonPojoUI.nom)
+            .setAutoCancel(true)
+            .setSmallIcon(fr.naitflo.pokedex.R.drawable.ic_pokeball)
+            .setColor(resources.getColor(fr.naitflo.pokedex.R.color.couleurPrincipale))
+            .setColorized(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(NOTIFICATION_ID, notificationCompat.build())
+        }
     }
 }
