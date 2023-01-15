46da74d47bbcd5a33f32247b1c645bfa3944dca1
Flo-a <73525632+Flo-a@users.noreply.github.com>
2023-01-15 18:04:11 +0100

    Ajout des icones résistances fiche de pokémon

--- app/src/main/java/fr/naitflo/pokedex/pokemon/view/activity/PokemonInfoActivity.kt
+++ app/src/main/java/fr/naitflo/pokedex/pokemon/view/activity/PokemonInfoActivity.kt
@@ -23,11 +23,11 @@ class PokemonInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPokemonInfoBinding.inflate(layoutInflater)
 
-         pokemon = if (Build.VERSION.SDK_INT >= 33) {
-             intent.getParcelableExtra("donneesPokemon", PokemonPojoUI::class.java)
-         }  else {
-             intent.getParcelableExtra("donneesPokemon")
-         }!!
+        pokemon = if (Build.VERSION.SDK_INT >= 33) {
+            intent.getParcelableExtra("donneesPokemon", PokemonPojoUI::class.java)
+        } else {
+            intent.getParcelableExtra("donneesPokemon")
+        }!!
 
         setContentView(binding.root)
         initBinding()
@@ -50,7 +50,7 @@ class PokemonInfoActivity : AppCompatActivity() {
             .load(pokemon.image)
             .into(binding.pokemonInfoImg)
 
-        binding.pokemonInfoNumero.text ="#"+pokemon.pokedexId.toString()
+        binding.pokemonInfoNumero.text = "#" + pokemon.pokedexId.toString()
 
         // Stats
         binding.pokemonInfoPv.text = pokemon.HP.toString()
@@ -79,6 +79,63 @@ class PokemonInfoActivity : AppCompatActivity() {
         binding.pokemonInfoResDragonTexte.text = pokemon.resistanceDragon.toString();
         binding.pokemonInfoResTenebreTexte.text = pokemon.resistanceTenebres.toString();
         binding.pokemonInfoResFeeTexte.text = pokemon.resistanceFee.toString();
+
+        //Icones Resistances
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/f/fb/Normal.png")
+            .into(binding.pokemonInfoResNormalIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/7/7f/Flying.png")
+            .into(binding.pokemonInfoResVolIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/3/30/Fire.png")
+            .into(binding.pokemonInfoResFeuIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/9/9d/Water.png")
+            .into(binding.pokemonInfoResEauIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/c/c5/Grass.png")
+            .into(binding.pokemonInfoResPlanteIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/0/05/Poison.png")
+            .into(binding.pokemonInfoResPoisonIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/3/30/Fighting.png")
+            .into(binding.pokemonInfoResCombatIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/2/21/Psychic.png")
+            .into(binding.pokemonInfoResPsyIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/8/8f/Ground.png")
+            .into(binding.pokemonInfoResSolIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/a/ab/Ghost.png")
+            .into(binding.pokemonInfoResSpectreIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/c/c9/Steel.png")
+            .into(binding.pokemonInfoResAcierIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/2/2f/Electric.png")
+            .into(binding.pokemonInfoResElectrikIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/0/0b/Rock.png")
+            .into(binding.pokemonInfoResRocheIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/7/77/Ice.png")
+            .into(binding.pokemonInfoResGlaceIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/c/c7/Dragon.png")
+            .into(binding.pokemonInfoResDragonIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/0/0e/Dark.png")
+            .into(binding.pokemonInfoResTenebreIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/4/43/Fairy.png")
+            .into(binding.pokemonInfoResFeeIcone)
+        Glide.with(this)
+            .load("https://static.wikia.nocookie.net/pokemongo/images/7/7d/Bug.png")
+            .into(binding.pokemonInfoResInsecteIcone)
+
 
         binding.pokemonInfoFermer.setOnClickListener {
             fermer()
